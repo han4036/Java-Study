d4cd4e8a2661b251ea3a0bd487d679e7b4f02dd4
han4036 <51283743+han4036@users.noreply.github.com>
2020-11-13 21:02:11 +0900

    java 기초
    자료형 변환

---
+++ ex07.java
@@ -0,0 +1,23 @@
+
+public class ex07 {
+
+	public static void main(String[] args) {
+		//자료형 변환
+		//1. charAt : 원하는 위치값을 문자로 변환한다.(자료형 Char)
+		//위치값은 0부터 시작
+		String msg = "승근이";
+		char ch = msg.charAt(0);//승을 출력
+		System.out.println(ch);
+		System.out.println();
+		
+		//2. length() : 문자열의 길이를 정수로 나타낸다. (자료형 int)
+		int su = msg.length();
+		System.out.println(su);//3이출력된다 (msg)
+		System.out.println();
+		
+		//3. contact(String) : 기존의 문자열에 문자열을 추가하는것 (문자열 연결)
+		
+			
+	}
+
+}
---
+++ ex08.java
@@ -0,0 +1,9 @@
+
+public class ex08 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
