8519c153f5aaf364b4e43874d3bb9d428cdd0b80
han4036 <51283743+han4036@users.noreply.github.com>
2020-11-13 19:08:13 +0900

    Add files via upload
    switch문 사용법 및 주의점추가

--- ex04.java
+++ ex04.java
@@ -2,8 +2,41 @@
 public class ex04 {
 
 	public static void main(String[] args) {
+		//switch문은 if문과 비슷한형식, 변수를 정하고 변수 값에따라 결과를 다르게 출력하기위해 사용한다
+		//코딩에따라 if문이 비효율적인경우 switch문을 쓰는경우도 있다
+		/* switch(변수){
+		 * case a : 출력값;
+		 * case b : 출력값;
+		 * case c : 출력값;
+		 * case d : 출력값;
+		 * default : 출력값; (default는 굳이 안써도 무방)
+		 }*/
+
+		//변수설정
+		//int tire = 3;
+		//String str;
+		//스위치문, case 뒤에는 변수값을 입력해준다.
+		/*switch(tire) {
+		case 1 : str ="외발자전거";
+		case 2 : str ="두발자전거";
+		case 3 : str ="세발자전거";
+		case 4 : str ="네발자전거";
+		default : str ="자전거가 아님";
+		}
+		System.out.println(str);*/
+		//break를 걸지않으면 계속 스위치문이 돌아가므로 오류가난다
+		//break를 걸면 switch문이 실행중 변수값을 만나면 switch문을 빠져나오는 알고리즘
 		
-
-	}
-
+			int tire = 3;
+			String str;
+				
+			switch(tire) {
+			case 1 : str ="외발자전거";break;
+			case 2 : str ="두발자전거";break;
+			case 3 : str ="세발자전거";break;
+			case 4 : str ="네발자전거";break;
+			default : str ="자전거가 아님";
+			}
+			System.out.println(str);
+}
 }
---
+++ ex05.java
@@ -0,0 +1,19 @@
+
+public class ex05 {
+
+	public static void main(String[] args) {
+		//변수 설정
+		String ride = "자전거";
+		String str;
+		
+		//스위치문
+		switch(ride) {
+		case "외발자전거" : str = "바퀴가 한개";break;
+		case "자전거" : str = "바퀴가 두개";break;
+		case "세발자전거" : str = "바퀴가 세개";break;
+		case "네발자전거" : str = "바퀴가 네개";break;
+		default : str = "자전거가 아님";
+		}
+		System.out.println(str);
+	}
+}
